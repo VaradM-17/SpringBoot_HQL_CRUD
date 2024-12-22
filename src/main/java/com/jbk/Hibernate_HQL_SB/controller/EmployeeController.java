c049e26755012328311787520e0539c595ae1453
VaradM-17 <varadmule17@gmail.com>
2024-12-22 15:56:26 +0530

    controller insert added

---
+++ src/main/java/com/jbk/Hibernate_HQL_SB/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.jbk.Hibernate_HQL_SB.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jbk.Hibernate_HQL_SB.entity.Employee;
+import com.jbk.Hibernate_HQL_SB.service.EmployeeService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeservice;
+
+    @PostMapping("/insertdata")
+    public String insertData(@RequestBody Employee e) {
+        String msg = employeeservice.insertData(e);
+        return msg;
+    }
+
+    
+}
