fcb02bd36d4538e73f0e3aeeaf8efa27691123c8
VaradM-17 <varadmule17@gmail.com>
2024-12-22 15:49:47 +0530

    service insert added

---
+++ src/main/java/com/jbk/Hibernate_HQL_SB/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.jbk.Hibernate_HQL_SB.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jbk.Hibernate_HQL_SB.dao.EmployeeDao;
+import com.jbk.Hibernate_HQL_SB.entity.Employee;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    public String insertData(Employee employee) 
+    {
+        return employeeDao.insertData(employee); 
+    }
+
+   
+}
